f6d0de2ebea51d14668e3f7e7a03ec58105a2201
Aluno <Aluno@10.85.198.10>
2024-06-19 15:51:15 -0300

    ArrayListEx2

---
+++ src/ArrayListEx2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListEx2 {
+    public static void main(String args[]) {
+ new ArrayList<>();
+        for(int i = 1; i < 10; i++){
+            System.out.println(i);
+        }
+        
+        
+        
+        
+         
+    }
+}
